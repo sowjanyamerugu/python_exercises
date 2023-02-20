0a7a9cd6c71e44ebed5f44274601020e65fb352c
Sowjanya merugu <sowjanya.merugu@gmail.com>
2023-02-20 16:12:09 -0500

    sample python exercises

---
+++ circle_radius.py
@@ -0,0 +1,8 @@
+# get the radius from the user
+radius = float(input("Enter the radius of the circle: "))
+
+# calculate the area of the circle
+area = 3.14159 * radius ** 2
+
+# print the result
+print("The area of the circle is:", area)
---
+++ last_firstname.py
@@ -0,0 +1,8 @@
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Use string formatting to print the names in reverse order
+print("{} {}".format(last_name, first_name))
+
+
+
